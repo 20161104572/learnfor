c3aa8d21ab3c8e0fe57e75b3e4c36d88f83a50df
20161104572 <2606612104@qq.com>
2018-09-19 11:21:04 +0800

    leijia
    Drgreg

---
+++ learnfor/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  learnfor
+//
+//  Created by 20161104572 on 2018/9/14.
+//  Copyright © 2018年 20161104572. All rights reserved.
+//
+
+import Foundation
+
+
+var c = 0
+for num in 0...100
+{
+    c = c+num
+}
+print(c)
+
